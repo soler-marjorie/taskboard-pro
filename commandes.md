7a9dc2f5e25591f00a3d791989a6c36f8c968908
soler.marjorie <soler.marjorie81@gmail.com>
2025-11-28 10:42:17 +0100

    M1-Angular-Taskboard-pro

---
+++ commandes.md
@@ -0,0 +1,14 @@
+#Commandes Angular
+
+Créer un nouveau projet :
+ng new Taskboard-pro --router --style:css
+
+
+Lancer le serveur:
+ng serve
+
+
+Créer un composant: 
+ng g c <nom-du-composant>
+
+Ajouter le composant dans app.ts et ajouter la balise dans app.html
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' }, // racine → /home
+    { path: 'home', component: Home },
+    { path: 'about', component: About },
+];
